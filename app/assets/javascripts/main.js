6b8c7432db0127f05f869844c1bb29d0a7b8de32
danyip <95078748+danyip@users.noreply.github.com>
2022-02-01 21:39:35 +1100

    fixed up the applic html & built nav dropdowns

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,25 @@
+document.addEventListener("click", e => {
+  // console.log("click");
+  const isDropdownButton = e.target.matches('.dropdown-button');
+  // console.log(isDropdownButton);
+
+  if (!isDropdownButton && e.target.closest('.dropdown') != null) {
+    console.log("hello");
+    return
+  };
+
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest('.dropdown')
+    currentDropdown.classList.toggle('active')
+
+  }
+
+  document.querySelectorAll(".active").forEach(dropdown => {
+
+    if (dropdown === currentDropdown) return
+
+    dropdown.classList.remove('active')
+
+  })
+})
