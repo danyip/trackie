b3a362d05f5068fa14bdc157cd411bc5d53e1bd8
danyip <danielyip90@gmail.com>
2022-02-03 22:15:57 +1100

    fixed up button and link styling

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -3,9 +3,23 @@ class SkillsController < ApplicationController
   before_action :check_if_logged_in
 
   def new
+    @skill = Skill.new
   end
 
   def create
+
+    @skill = Skill.create skill_params
+
+    if @skill.persisted?
+
+      redirect_to skills_path
+
+    else # if create was unsuscessful, go back to the form
+
+      render :new
+    
+    end
+
   end
 
   def index
@@ -17,13 +31,35 @@ class SkillsController < ApplicationController
   end
 
   def edit
+    @skill = Skill.find params[:id]
   end
 
   def update
+
+    @skill = Skill.find params[:id]
+
+
+    if @skill.update skill_params # if the task update is sucessful
+        
+      redirect_to skill_path params[:id] # redirect to the task
+    else #if update unsucessful
+      render :edit # back to the edit page
+    end 
+
   end
 
   def destroy
     Skill.destroy params[:id]
     redirect_to skills_path
   end
+
+
+  private
+
+  def skill_params
+    params
+      .require(:skill)
+        .permit(:skill_type)
+  end
+
 end
