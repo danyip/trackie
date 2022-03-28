8f1d2e35b0bd3d190781b57d5591316363133ba0
danyip <hello@danyip.com>
2022-03-28 22:22:54 +1100

    added conditional to create button

--- app/assets/javascripts/add.js
+++ app/assets/javascripts/add.js
@@ -1,13 +1,16 @@
 //  Duplicate the fields inside of the nested form when the add button is clicked
   const createButton = document.getElementById("add-skill");
 
-  createButton.addEventListener("click", function(){
+  if(createButton){
 
-    const lastId = document.querySelector('#fieldset-container').lastElementChild.id;
-    const newId = parseInt(lastId, 10) + 1;
-    const newFieldset = document.querySelector('[id="0"]').outerHTML.replace(/0/g,newId)
-
-    document.querySelector("#fieldset-container").insertAdjacentHTML(
-      "beforeend", newFieldset
-    );
-  })
+    createButton.addEventListener("click", function(){
+      
+      const lastId = document.querySelector('#fieldset-container').lastElementChild.id;
+      const newId = parseInt(lastId, 10) + 1;
+      const newFieldset = document.querySelector('[id="0"]').outerHTML.replace(/0/g,newId)
+      
+      document.querySelector("#fieldset-container").insertAdjacentHTML(
+        "beforeend", newFieldset
+        );
+      })
+  }
